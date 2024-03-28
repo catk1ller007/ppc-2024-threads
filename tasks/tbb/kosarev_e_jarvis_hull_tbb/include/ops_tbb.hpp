c8aedf401b247c10cb9d7ba1783b4f9db42d120b
Zhenya_Kulaev <kylaev11@gmail.com>
2024-03-29 00:48:58 +0300

    first commit

---
+++ tasks/tbb/kosarev_e_jarvis_hull_tbb/include/ops_tbb.hpp
@@ -0,0 +1,50 @@
+// Copyright 2024 Kosarev Egor
+#pragma once
+
+#include <algorithm>
+#include <iostream>
+#include <stack>
+#include <string>
+#include <vector>
+
+#include "core/task/include/task.hpp"
+
+struct Point {
+  int x, y;
+
+  bool operator==(const Point& other) const { return x == other.x && y == other.y; }
+};
+
+class TestTaskSequentialKosarevJarvisHull : public ppc::core::Task {
+ public:
+  explicit TestTaskSequentialKosarevJarvisHull(std::shared_ptr<ppc::core::TaskData> taskData_)
+      : Task(std::move(taskData_)) {}
+  bool pre_processing() override;
+  bool validation() override;
+  bool run() override;
+  bool post_processing() override;
+
+ private:
+  std::vector<Point> points;
+  std::vector<Point> pointsHull;
+};
+
+class TestTBBTaskParallelKosarevJarvisHull : public ppc::core::Task {
+ public:
+  explicit TestTBBTaskParallelKosarevJarvisHull(std::shared_ptr<ppc::core::TaskData> taskData_)
+      : Task(std::move(taskData_)) {}
+  bool pre_processing() override;
+  bool validation() override;
+  bool run() override;
+  bool post_processing() override;
+
+ private:
+  std::vector<Point> points;
+  std::vector<Point> pointsHull;
+};
+
+int orientation(Point p, Point q, Point r);
+
+double distance(Point p1, Point p2);
+
+std::vector<Point> JarvisAlgo(const std::vector<Point>& arrPoints);
